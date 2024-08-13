fc0c97bdbb589f23d86df24b2b6ca80df4023170
Lucas Cavalcante <lucas.cavalcante.dev@icloud.com>
2024-08-13 10:17:33 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="dark min-h-screen bg-background font-sans text-foreground antialiased">
+      <h1>Teste Técnico - Tenda</h1>
+    </div>
+  );
+}
