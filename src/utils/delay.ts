5b9eaca10213a11b7a35deeeeaf067d6467b5f09
Lucas Cavalcante <lucas.cavalcante.dev@icloud.com>
2024-08-13 17:45:22 -0300

    docs: add docs in delay function

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -1,3 +1,11 @@
-export const delay = (ms = 1000) => {
+/**
+ * Creates a promise that resolves after a specified delay in milliseconds.
+ *
+ * @function delay
+ * @param {number} [ms=1000] - The delay time in milliseconds before the promise resolves.
+ *                             The default value is 1000ms (1 second).
+ * @returns {Promise<void>} A promise that resolves after the specified delay.
+ */
+export const delay = (ms: number = 1000): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
