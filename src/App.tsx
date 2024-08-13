fea23bd0961572b0edc441d706b5ad3a118a1e2e
Lucas Cavalcante <lucas.cavalcante.dev@icloud.com>
2024-08-13 16:17:58 -0300

    feat: add Sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import { queryClient } from '@/lib/query-client';
-import { Form } from '@/components/Form';
+import { Form, Toaster } from '@/components';
 
 export function App() {
   return (
@@ -9,6 +9,7 @@ export function App() {
       <div className="dark flex min-h-screen items-center justify-center bg-background font-sans text-foreground antialiased">
         <Form />
       </div>
+      <Toaster richColors />
     </QueryClientProvider>
   );
 }
