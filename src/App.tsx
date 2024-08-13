1d13b070297b32a9d06953fda48d10b48c7d3f6c
Lucas Cavalcante <lucas.cavalcante.dev@icloud.com>
2024-08-13 13:51:36 -0300

    feat: add and configure react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import { queryClient } from '@/lib/query-client';
 import { Form } from '@/components/Form';
 
 export function App() {
   return (
-    <div className="dark flex min-h-screen items-center justify-center bg-background font-sans text-foreground antialiased">
-      <Form />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="dark flex min-h-screen items-center justify-center bg-background font-sans text-foreground antialiased">
+        <Form />
+      </div>
+    </QueryClientProvider>
   );
 }
